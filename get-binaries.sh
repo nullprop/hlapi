367e9c6ecce1451c886a9192167ab98bdeca1a8c
Lauri Räsänen <git@nullprop.sh>
2025-05-12 22:04:28 +0300

    Use the windows version :(

--- get-binaries.sh
+++ get-binaries.sh
@@ -6,7 +6,7 @@ HLA="$HOME/.local/share/Steam/steamapps/common/Half-Life Alyx"
 
 mkdir -p bin
 
-cp "$HLA"/game/bin/linuxsteamrt64/*.so ./bin/
-cp "$HLA"/game/bin/linuxsteamrt64/hlvr ./bin/
-cp "$HLA"/game/hlvr/bin/linuxsteamrt64/*.so ./bin/
+cp "$HLA"/game/bin/win64/*.dll ./bin/
+cp "$HLA"/game/bin/win64/*.exe ./bin/
+cp "$HLA"/game/hlvr/bin/win64/*.dll ./bin/
 
